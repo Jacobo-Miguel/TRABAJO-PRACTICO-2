e2276000bd308c7650a1e298a05916055533234b
Jacobo-Miguel <65904635+Jacobo-Miguel@users.noreply.github.com>
2020-10-20 20:08:51 -0300

    Update ejercicio36.c

--- ejercicio36.c
+++ ejercicio36.c
@@ -21,86 +21,102 @@ typedef struct nodo{
     struct nodo *siguiente;
 }nodo;
 
-void insertarnodo(nodo *primero,nodo *ultimo,nodo *nuevo,datos aux);
-void mostrarlista(nodo *primero,nodo *actual);
+void mostrarlista(nodo *primero, FILE *contacto_t);
 int main(){
 	FILE *contacto,*contacto_t;
-	int pos;
 	datos aux;
-	nodo *primero,*ultimo,*nuevo,*actual;
-	primero=ultimo=nuevo=actual=NULL;
-	if((contacto=fopen("contactos.dat","rb+"))==NULL){
-		printf("No se pudo abrir el archivo!!");
-	}
+	nodo *primero,*ultimo,*r;
+	primero=ultimo=NULL;
 	if((contacto_t=fopen("contactos_ordenados.dat","wb"))==NULL){
 		printf("No se pudo abrir el archivo!!");
 	}
-	printf("REGISTROS EN EL ARCHIVO CONTACTOS.DAT\n");
-	fread(&aux,sizeof(aux),1,contacto);
-	while(!feof(contacto)){
-		printf("%s |   %s   |  %s  |   %d   |   %s   |  %s\n",aux.id,aux.nombre,aux.apellido,aux.edad,aux.telefono,aux.mail);
-		fwrite(&aux,sizeof(aux),1,contacto_t);
-		fflush(stdin);
-		printf("INGRESE EL NUEVO ID: ");
-	    scanf("%s",&aux.id);
-	    fflush(stdin);
-	    insertarnodo(primero,ultimo,nuevo,aux);
-	    pos=ftell(contacto_t)-sizeof(aux);
-        fseek(contacto_t,pos,SEEK_SET);
-	    fwrite(&aux,sizeof(aux),1,contacto_t);
-		fread(&aux,sizeof(aux),1,contacto);
-	}
-	fclose(contacto_t);
-	system("pause");
-	printf("\n");
-	if((contacto_t=fopen("contactos_ordenados.dat","rb+"))==NULL){
+	if((contacto=fopen("contactos.dat","rb+"))==NULL){
 		printf("No se pudo abrir el archivo!!");
 	}
-	printf("REGISTROS EN EL ARCHIVO CONTACTOS_ORDENADOS.DAT\n");
-	printf("ID    |   NOMBRE   | APELLIDO |  EDAD  |    TELEFONO    |    MAIL\n");
-	printf("--------------------------------------------------------------------\n");
-	fread(&aux,sizeof(aux),1,contacto_t);
-	while(!feof(contacto_t)){
-		printf("%s     |   %s   |  %s  |   %d   |   %s   |  %s\n",aux.id,aux.nombre,aux.apellido,aux.edad,aux.telefono,aux.mail);	
-		fread(&aux,sizeof(aux),1,contacto_t);
-	}
-    mostrarlista(primero,actual);
+	else{
+		printf("REGISTROS EN EL ARCHIVO CONTACTOS.DAT\n");
+	    fread(&aux,sizeof(aux),1,contacto);
+	    while(!feof(contacto)){
+	    	printf("%s |   %s   |  %s  |   %d   |   %s   |  %s\n",aux.id,aux.nombre,aux.apellido,aux.edad,aux.telefono,aux.mail);
+	    	nodo *nuevo = (nodo*)malloc(sizeof(nodo));
+	    	fflush(stdin);
+	    	printf("Ingrse el id nuevo: ");
+	    	scanf("%s",&aux.id);
+	     	strcpy(nuevo->datos_t.id,aux.id);
+	        strcpy(nuevo->datos_t.nombre,aux.nombre);
+	        strcpy(nuevo->datos_t.apellido,aux.apellido);
+	        nuevo->datos_t.edad = aux.edad;
+	        strcpy(nuevo->datos_t.telefono,aux.telefono);
+	        strcpy(nuevo->datos_t.mail,aux.mail);
+	
+	        if(primero == NULL){
+		        primero = nuevo;
+		        nuevo->siguiente = NULL;
+		        ultimo = nuevo;
+	        }
+	        else{
+	        	r = primero;
+	    	    while(1){
+	    	    	if(r->datos_t.nombre < nuevo->datos_t.nombre){
+	    			    nuevo->siguiente = primero;
+	    		    	primero = nuevo;
+	    		    	break;
+				    }
+				while(r->siguiente!=NULL){
+					if(r->siguiente->datos_t.nombre < nuevo->datos_t.nombre){
+						r = r->siguiente;
+					}
+					else{
+						break;
+					}
+				}
+				if(r==ultimo){
+					ultimo->siguiente = nuevo;
+					ultimo = nuevo;
+					ultimo->siguiente = NULL;
+					break;
+				}
+				nuevo->siguiente = r->siguiente;
+				r->siguiente = nuevo;
+				break;
+			}
+	    }
+		fread(&aux,sizeof(aux),1,contacto);
+	}	
 	fclose(contacto);
+	mostrarlista(primero,contacto_t);
+	fclose(contacto_t);
+	}
+	
+	system("pause");
+	printf("\n");
+	if((contacto_t=fopen("contactos_ordenados.dat","rb"))==NULL){
+		printf("No se pudo abrir el archivo!!");
+	}
+	else{
+		printf("REGISTROS EN EL ARCHIVO CONTACTOS_ORDENADOS.DAT\n");
+	    printf("ID    |   NOMBRE   | APELLIDO |  EDAD  |    TELEFONO    |    MAIL\n");
+	    printf("--------------------------------------------------------------------\n");
+	    fread(&aux,sizeof(aux),1,contacto_t);
+    	while(!feof(contacto_t)){
+		    printf("%s     |   %s   |  %s  |   %d   |   %s   |  %s\n",aux.id,aux.nombre,aux.apellido,aux.edad,aux.telefono,aux.mail);	
+		    fread(&aux,sizeof(aux),1,contacto_t);
+    	}
+    	
+	}
 	fclose(contacto_t);
 	return 0;
 }
 
-void insertarnodo(nodo *primero,nodo *ultimo,nodo *nuevo,datos aux){
-	nuevo = (nodo*)malloc(sizeof(nodo));
-	strcpy(nuevo->datos_t.nombre,aux.nombre);
-	strcpy(nuevo->datos_t.apellido,aux.apellido);
-	nuevo->datos_t.edad = aux.edad;
-	strcpy(nuevo->datos_t.telefono,aux.telefono);
-	strcpy(nuevo->datos_t.mail,aux.mail);
-	
-	if(primero == NULL){
-		primero = nuevo;
-		nuevo->siguiente = NULL;
-		ultimo = nuevo;
-		printf("NODO CREADO CORRECTAMENTE\n");
-	}
-	else{
-		ultimo->siguiente = nuevo;
-		nuevo->siguiente = NULL;
-		ultimo = nuevo;
-		printf("NODO CREADO CORRECTAMENTE(2)\n");
-	}
-}
-
-void mostrarlista(nodo *primero,nodo *actual){
-	actual = (nodo*)malloc(sizeof(nodo));
+void mostrarlista(nodo *primero, FILE *contacto_t){
+	nodo *actual = (nodo*)malloc(sizeof(nodo));
 	actual = primero;
 	if(primero != NULL){
+		printf("LISTA DE CONTACTOS\n");
 		while(primero != NULL){
-			printf("LISTA DE CONTACTOS\n");
 			printf("%s |   %s   |  %s  |   %d   |   %s   |  %s\n",actual->datos_t.id,actual->datos_t.nombre,actual->datos_t.apellido,actual->datos_t.edad,actual->datos_t.telefono,actual->datos_t.mail);
-			free(actual);
-            actual = actual->siguiente;
+			fwrite(&actual->datos_t,sizeof(actual->datos_t),1,contacto_t);
+	    	actual = actual->siguiente;
 		}
 	}
 	else{
